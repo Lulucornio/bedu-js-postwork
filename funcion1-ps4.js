bb93fa217ace3616cfe1266a940cb20b589e8b59
Luu Luu <luu7755@gmail.com>
2021-07-31 13:46:06 -0500

    deep equal con if para longitud 2

--- funcion1-ps4.js
+++ funcion1-ps4.js
@@ -1,9 +1,15 @@
 function deepEqual(a, b) {
-  let equal;
+
   if (typeof a === 'object' && typeof b === 'object'){
+    if (Object.keys(a).length === Object.keys(b).length){
+     
       for (let keys in a){
             equal = deepEqual(a[keys], b[keys]);
             if (!equal) break; 
+        }
+    }
+    else {
+        equal = false;
       }
   }
   else {
@@ -12,6 +18,7 @@ function deepEqual(a, b) {
 
   return equal;
 }
+
 
 var john = {
   firstName: 'John',
@@ -22,5 +29,6 @@ var john = {
 console.log('Test 1:', deepEqual(1, 1)) // true
 console.log('Test 2:', deepEqual(1, '1')) // false
 console.log('Test 3:', deepEqual(john, john)) // true
-console.log('Test 4:', deepEqual(john, { firstName: 'John', lastName: 'Doe' })) // true
+console.log('Test 4:', deepEqual(john, { firstName: 'John', lastmiau: 'Doe' })) // true
 console.log('Test 5:', deepEqual(john, { firstName: 'John' })) // false
+
