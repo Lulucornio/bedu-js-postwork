9a40e01d49ff7cc9a30e0e360f727c511ce60796
Luu Luu <luu7755@gmail.com>
2021-08-05 15:24:57 -0500

    actualización general 2

--- funcion1-ps4.js
+++ funcion1-ps4.js
@@ -30,6 +30,6 @@ var john = {
 console.log('Test 1:', deepEqual(1, 1)) // true
 console.log('Test 2:', deepEqual(1, '1')) // false
 console.log('Test 3:', deepEqual(john, john)) // true
-console.log('Test 4:', deepEqual(john, { firstName: 'John', lastmiau: 'Doe' })) // true
+console.log('Test 4:', deepEqual(john, { firstName: 'John', lastName: 'Doe' })) // true
 console.log('Test 5:', deepEqual(john, { firstName: 'John' })) // false
 
